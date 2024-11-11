b460d858dd2db76ea5b45f93d1566fd7d51a9b85
privelosos <priscilavelosoconsultoria@gmail.com>
2024-11-11 18:26:15 -0400

    capturei elementos postAutor

--- conexao-html-js/script.js
+++ conexao-html-js/script.js
@@ -1,1 +1,5 @@
-console.log("Hello world!")
+let logo = document.getElementById("logo")
+console.log(logo)
+
+let postAutores = document.getElementsByClassName("post-autor")
+console.log(postAutores)
