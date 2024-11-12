6ff2c15d6a95519be81c1e9019d4823d0256d9d0
privelosos <priscilavelosoconsultoria@gmail.com>
2024-11-11 20:03:29 -0400

    capturei formulario e section com ambos os posts

--- conexao-html-js/script.js
+++ conexao-html-js/script.js
@@ -1,5 +1,33 @@
 let logo = document.getElementById("logo")
-console.log(logo)
+//console.log(logo)
 
 let postAutores = document.getElementsByClassName("post-autor")
-console.log(postAutores)
+//console.log(postAutores)
+
+let texto = document.querySelector("#post02 .post-texto")
+//console.log(post02)
+
+let listaRedesFooter = document.querySelectorAll("footer .lista_redes li")
+//console.log (listaRedesFooter)
+
+function percorrerArray(lista){
+    for(let i = 0; i < lista.length; i ++){
+        console.log(lista[1])
+}
+  }
+
+percorrerArray(postAutores)
+
+const formulario = document.getElementById("formulario");
+console.log(formulario);
+
+ 
+const postsSection = document.getElementById("posts");
+console.log(postsSection);
+
+
+
+
+
+
+
